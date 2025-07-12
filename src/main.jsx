f49a4cb6ccf3bbc9409363067fe8e7c654798b64
Shagun Singh <shagunthakur255@gmail.com>
2025-07-12 22:56:58 +0530

    Debug logs: verify loader unmount

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import LoaderWrapper from "./LoaderWrapper.jsx";   // ← add this line
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoaderWrapper>          {/* 🔒 shows loader until ready */}
       <App />
-    </LoaderWrapper>
   </React.StrictMode>
 );
