dc602b25b9580c1596ec050e83ce618e26899fa5
Shagun Singh <167783822+Shagunsingh18@users.noreply.github.com>
2025-07-12 14:43:00 +0530

    Add files via upload

---
+++ loader.jsx
@@ -0,0 +1,12 @@
+import { FaSpinner } from "react-icons/fa";   // any icon works
+import "./loader.css";
+
+export default function Loader() {
+  return (
+    <div id="preloader">
+      <FaSpinner className="loader-icon" />
+      <p className="loading-text">Loading Shagun’s portfolio…</p>
+    </div>
+  );
+}
+
